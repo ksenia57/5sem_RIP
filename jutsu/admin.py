5a521ac2f16627fa02be90f921db6ae3eeec831d
ksenia57 <55061224+ksenia57@users.noreply.github.com>
2021-01-18 18:26:07 +0300

    Add files via upload

---
+++ jutsu/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Anime, Сharacter, Anime_Genre, Genre
+
+admin.site.register(Anime)
+admin.site.register(Сharacter)
+admin.site.register(Anime_Genre)
+admin.site.register(Genre)
---
+++ jutsu/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class JutsuConfig(AppConfig):
+    name = 'jutsu'
